aa20d51285950239dbb9b6dc45671df7a553ecb9
Fatima Malalla <FatimaMallala.04@gmail.com>
2024-12-03 17:38:36 +0300

    -

---
+++ BetterJobzApp/BetterJobzApp/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  BetterJobzApp
+//
+//  Created by BP-36-201-01 on 03/12/2024.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
