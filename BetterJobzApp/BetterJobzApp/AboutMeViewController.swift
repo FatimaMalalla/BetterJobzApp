f3a4ef40ca689b1b98059391254e45745d09d893
Fatima Malalla <FatimaMallala.04@gmail.com>
2024-12-12 16:48:26 +0300

    CVTool updates

---
+++ BetterJobzApp/BetterJobzApp/AboutMeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutMeViewController.swift
+//  BetterJobzApp
+//
+//  Created by BP-36-201-01 on 12/12/2024.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
+class AboutMeViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BetterJobzApp/BetterJobzApp/CVToolViewController.swift
+++ BetterJobzApp/BetterJobzApp/CVToolViewController.swift
@@ -9,69 +9,8 @@ import UIKit
 
 class CVToolViewController: ViewController , UITextViewDelegate {
 
+  
     
    
    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        print("textView: \(textView != nil), placeholderLabel: \(placeholderLabel != nil)")
-//          
-//          textView.delegate = self
-//          placeholderLabel.isHidden = !textView.text.isEmpty
-    }
-    
-    @IBAction func CancelBttnTapped(_ sender: UIButton) {
-        
-        // Dismiss the current view controller or navigate back to the main page
-               self.dismiss(animated: true, completion: nil)
-        
-    }
-    
-    @IBAction func saveBtnTapped(_ sender: UIButton) {
-        
-//        guard let text = textView.text, !text.isEmpty else {
-//                    // Show an alert if there's no text to save
-//                    let alert = UIAlertController(
-//                        title: "No Content",
-//                        message: "Please add details before saving.",
-//                        preferredStyle: .alert
-//                    )
-//                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-//                    present(alert, animated: true, completion: nil)
-//                    return
-//                }
-//
-//                // Save the text content (could save to UserDefaults, CoreData, or pass to the main screen)
-//                saveTextData(text)
-//
-//                // Dismiss the current view controller or navigate back to the main page
-//                self.dismiss(animated: true, completion: nil)
-        
-    }
-    
-   
-    
-//    func textViewDidBeginEditing(_ textView: UITextView) {
-//            // Show the placeholder when user starts editing and text is empty
-//            if textView.text.isEmpty {
-//                placeholderLabel.isHidden = false
-//            }
-//        }
-//
-//        func textViewDidChange(_ textView: UITextView) {
-//            // Hide the placeholder when user starts typing
-//            placeholderLabel.isHidden = !textView.text.isEmpty
-//        }
-//
-//        func textViewDidEndEditing(_ textView: UITextView) {
-//            // Show placeholder if text view is empty when editing ends
-//            placeholderLabel.isHidden = !textView.text.isEmpty
-//        }
-//    
-//
-//    // MARK: - Save Text Data
-//       func saveTextData(_ text: String) {
-//           // Example: Save to UserDefaults (you can replace this with your preferred data-saving method)
-//           UserDefaults.standard.set(text, forKey: "savedCVText")
-//       }
 }
